953009dab67b15ab95ece86e02ad111f3353f834
Pavitra Behre <pavitra.behre@gmail.com>
2018-04-07 22:22:08 +0530

    Added feed_box template which will be later used for dynamic feed generation

---
+++ templates/feed_box.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pavitra14
+ * Date: 7/4/18
+ * Time: 10:02 PM
+ */
+?>
+<div class="box box-success">
+    <div class="box-header with-border">
+        <h3 class="box-title">{{FROM}} -> {{TO}}</h3>
+    </div>
+    <div class="box-body">
+        {{CONTENT}}
+    </div>
+    <!-- /.box-body -->
+    <div class="box-footer">
+        <div class="row">
+            <div class="col-xs-2"><a href="#">Like</a></div>
+        </div>
+    </div>
+    <!-- /.box-footer -->
+</div>
+<!-- /.box -->
--- views/feed.php
+++ views/feed.php
@@ -146,36 +146,16 @@ $fname = $arr_details['fname'];
             <!-- Content Header (Page header) -->
             <section class="content-header">
                 <h1>
-                    Message Board
+                    Dab Board
                     <small>Feed</small>
                 </h1>
             </section>
 
             <!-- Main content -->
             <section class="content">
-                <div class="callout callout-info">
-                    <h4>Tip!</h4>
-
-                    <p>Add the layout-top-nav class to the body tag to get this layout. This feature can also be used with a
-                        sidebar! So use this class if you want to remove the custom dropdown menus from the navbar and use regular
-                        links instead.</p>
-                </div>
-                <div class="callout callout-danger">
-                    <h4>Warning!</h4>
-
-                    <p>The construction of this layout differs from the normal one. In other words, the HTML markup of the navbar
-                        and the content will slightly differ than that of the normal layout.</p>
-                </div>
-                <div class="box box-default">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Blank Box</h3>
-                    </div>
-                    <div class="box-body">
-                        The great content goes here
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
+                <?php
+                require_once 'templates/feed_box.php';
+                ?>
             </section>
             <!-- /.content -->
         </div>
