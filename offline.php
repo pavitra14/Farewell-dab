82a0e8fd7494c60db4f5873d981f72efdca65a7d
Pavitra Behre <pavitra.behre@gmail.com>
2018-04-14 11:47:24 +0530

    Fix - ServiceWorker

--- offline.php
+++ offline.php
@@ -1,8 +1,3 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
