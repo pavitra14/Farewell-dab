2c1c665471961fead84c708c115101a84d67b6f9
Pavitra Behre <pavitra.behre@gmail.com>
2018-04-16 10:42:46 +0530

    Added a config file.

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pavitra14
+ * Date: 16/4/18
+ * Time: 10:29 AM
+ */
+//This file contains all the constants, and definitions
+define('LTE', 'modules/adminlte/');
+define('SITE_URL', 'https://farewell.pbehre.in/');
+/**
+ * Database details
+ */
+$servername = "localhost";
+$username = "pbehrein_main";
+$password = "Pavitra14";
+$dbname = "pbehrein_farewell";
